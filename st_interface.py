2ba7552bcde76889f2aae36d26751ce69e657d9d
Dragonborn <78375175+MLDovakin@users.noreply.github.com>
2023-01-07 20:55:09 +0300

    Add files via upload

---
+++ st_interface.py
@@ -0,0 +1,50 @@
+import PIL
+from sahi.utils.yolov5 import (
+    download_yolov5s6_model,
+)
+import sahi
+import streamlit as st
+from sahi import AutoDetectionModel
+from sahi.utils.cv import read_image
+from sahi.utils.file import download_from_url
+from sahi.predict import get_prediction, get_sliced_prediction, predict
+
+
+
+detection_model = AutoDetectionModel.from_pretrained(
+    model_type='yolov5',
+    model_path='./last_st.pt',
+    confidence_threshold=0.3,
+    device="cpu",)
+
+
+def define_doc_state(doc):
+
+    with open(doc.name,"wb") as f:
+         f.write(doc.getbuffer())
+
+    result = get_sliced_prediction(
+        doc.name,
+        detection_model,
+        slice_height=250,
+        slice_width=250,
+        postprocess_match_threshold=0.45,
+        overlap_height_ratio=0.6,
+        overlap_width_ratio=0.6
+    )
+    result.export_visuals(export_dir='./')
+
+
+
+
+
+uploaded_file = st.file_uploader("Выберите файл", type=[".JPG", ".jpg", ".png",], accept_multiple_files=False)
+if uploaded_file:
+    st.success("Файл успешно загружен", icon="✅")
+    define_button_state = st.button("Определить")
+    if define_button_state:
+         define_doc_state(uploaded_file)
+         res_image = PIL.Image.open('./prediction_visual.png')
+         st.image(res_image, caption='Detection')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
